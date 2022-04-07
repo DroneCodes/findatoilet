65e7d4b90cdafe1e45f5e9afb1d5e779b22b9bb0
Hp <oluwafisayomiogunyemi@gmail.com>
2022-04-08 00:43:14 +0100

    with Hendrick

--- lib/location/location_service.dart
+++ lib/location/location_service.dart
@@ -9,10 +9,10 @@ class LocationService {
   Future<String> getPlaceId(String input) async {
     final String url =
         'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=$input&inputtype=textquery&key=$key';
-
     var response = await http.get(Uri.parse(url));
-
     var json = convert.jsonDecode(response.body);
+    print(json);
+
 
     var placeId = json['candidates'][0]['place_id'] as String;
 
@@ -25,13 +25,11 @@ class LocationService {
         'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$key';
 
     var response = await http.get(Uri.parse(url));
-
     var json = convert.jsonDecode(response.body);
 
     var results = json['result'] as Map<String, dynamic>;
 
     print(results);
-    return(results);
-
+    return (results);
   }
 }
